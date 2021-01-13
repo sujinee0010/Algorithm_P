efeced27a2b075878506380723ef012bc47410ce
sujin <sujinee9797@gmail.com>
2021-01-14 00:24:26 +0900

    210113_실전알고리즘_금광
    DP

--- algorithmByNDB/getGold.py
+++ algorithmByNDB/getGold.py
@@ -1,20 +1,30 @@
 t = int(input())
+result = [0]*t
+for i in range(t):
+    n, m = map(int, input().split())
+    dp = []
+    array = list(map(int, input().split()))
 
-result = []
+    arr = []
+    index = 0
+    for _ in range(n):
+        dp.append(array[index:index + m])
+        index += m
+
+    # 열 기준
+    for col in range(1, m):
+        for row in range(n):
+            org_num = dp[row][col]
+            dp[row][col] += dp[row][col - 1]
+            if row - 1 >= 0:
+                dp[row][col] = max(dp[row][col], dp[row - 1][col - 1] + org_num)
+            if row + 1 < n:
+                dp[row][col] = max(dp[row][col], dp[row + 1][col - 1] + org_num)
+
+    for row in range(n):
+        result[i] = max(result[i], dp[row][m - 1])
+
 
 for i in range(t):
-    n , m = map(int , input().split())
-    nums = list(map(int ,input().split()))
-
-    arr =[]
-
-    for i in range(len(nums)):
-        row =[]
-        for j in range(m): # 4 개씩 자른다 ?
-            row.append(nums[i])
-    dp =[]
-    # 열 기준
-    for i in range(m):
-        for j in range(n):
-            dp[i] = max(p[i-1]+ arr[j][i])
+    print(result[i])
 
