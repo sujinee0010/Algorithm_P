7e851575409552e51f10bf2a5e69e59c3252718b
sujin <sujinee9797@gmail.com>
2021-01-27 01:54:11 +0900

    210127_boj_1759_암호만들기
    백트랙킹

--- BOJ/makePWD.py
+++ BOJ/makePWD.py
@@ -4,7 +4,8 @@ dfs 돌기
 답 기록 조건은 최종 조합 문자가  l글자일 경우
 dfs  돌떄 체 크해야되는거
 - 전의 알파벳이면 안됌
-- 이미 선택한 알파벳이며 안됌  + 모음 하나 있는지 , 자음 두개 있는지
+- 이미 선택한 알파벳이며 안됌   --> 오름 차순 후 그 다음 인덱스부터 순회 하는 것으로 해결 가능
+- 모음 하나 있는지 , 자음 두개 있는지 check
 """
 import sys
 
@@ -12,22 +13,15 @@ l, c = map(int, sys.stdin.readline().split())
 alpa = list(map(str, sys.stdin.readline().split()))
 
 alpa.sort()
-result = []
 
-def makePWD(cnt, alpa, idx, visited, pwd, l, c):
+def makePWD(cnt, alpa, idx, pwd, l, c):
     if len(pwd) == l:
-        m = sum(list(map(lambda x: 1 if x in pwd else 0, ['a', 'e', 'i', 'o','u'])))
-        if m > 0 and len(pwd)-m >1:
+        m = sum(list(map(lambda x: 1 if x in pwd else 0, ['a', 'e', 'i', 'o', 'u'])))
+        if m > 0 and len(pwd) - m > 1:
             print(pwd)
     else:
-        for i in range(len(alpa)):
-            if visited[i] == False and idx < i:
-                visited[i] = True
-                makePWD(cnt + 1, alpa, i, visited, pwd + alpa[i], l, c)
-                visited[i] = False
+        for i in range(idx, len(alpa)):
+            makePWD(cnt + 1, alpa, i+1, pwd + alpa[i], l, c)
 
 
-visited = [False] * c
-makePWD(0, alpa, -1, visited, "", l, c)
-
-
+makePWD(0, alpa, 0, "", l, c)
