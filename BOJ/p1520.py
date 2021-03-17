dfb5416bd79f533860558d516f5a48896adaa360
sujin <sujinee9797@gmail.com>
2021-03-17 17:19:58 +0900

    210317_boj_1520_내리막길
    DFS+DP

--- BOJ/p1520.py
+++ BOJ/p1520.py
@@ -1,8 +1,11 @@
+from sys import setrecursionlimit
 import sys
+
+setrecursionlimit(10 ** 9)  # 재귀 한도 늘려주기
 
 n, m = map(int, sys.stdin.readline().split())
 nums = []
-visited = [[False] * m for _ in range(n)]
+dp = [[-1] * m for _ in range(n)]
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 ans = 0
@@ -10,21 +13,20 @@ for i in range(n):
     nums.append(list(map(int, sys.stdin.readline().split())))
 
 
-def dfs(i, j, nums, visited):
-    ans = 0
-    if i == n - 1 and j == m - 1:
+def dfs(i, j, nums, dp):
+    if i == 0 and j == 0:
         return 1
-    else:
-        for _ in range(4):
-            nx = i + dx[_]
-            ny = j + dy[_]
-            if 0 <= nx < len(nums) and 0 <= ny < len(nums[0]) and visited[nx][ny] == False and nums[nx][ny] < nums[i][
-                j]:
-                visited[nx][ny] = True
-                ans += dfs(nx, ny, nums, visited)
-                visited[nx][ny] = False
-        return ans
+    if dp[i][j] != -1:
+        return dp[i][j]
+
+    dp[i][j] = 0
+    for _ in range(4):
+        nx = i + dx[_]
+        ny = j + dy[_]
+        if 0 <= nx < len(nums) and 0 <= ny < len(nums[0]) \
+                and nums[nx][ny] > nums[i][j]:
+            dp[i][j] += dfs(nx, ny, nums, dp)
+    return dp[i][j]
 
 
-visited[0][0] = True
-print(dfs(0, 0, nums, visited))
+print(dfs(n - 1, m - 1, nums, dp))
