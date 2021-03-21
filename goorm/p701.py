d97450d6cf6164532caf15fdbd628bab9265d3c4
sujin <sujinee9797@gmail.com>
2021-03-22 00:02:29 +0900

    210321_goorm_보조배터리
    우큐+ 탐욕법

--- goorm/p701.py
+++ goorm/p701.py
@@ -6,11 +6,10 @@ info = int(sys.stdin.readline())
 btr = []
 btr_5 = []
 btr_8 = []
-
+cnt=0
 sum = 0
 for _ in range(info):
     val, type = map(int, sys.stdin.readline().split())
-    heapq.heappush(btr, (val, type))
     if type == 0:
         heapq.heappush(btr_5, (val, type))
     else:
@@ -18,8 +17,27 @@ for _ in range(info):
 
 for i in range(n5):
     if btr_5:
-        sum += heapq.heappop(btr_5)
+        cnt+=1
+        sum += heapq.heappop(btr_5)[0]
+
+for i in range(len(btr_5)):
+    val ,type = btr_5[i]
+    heapq.heappush(btr,(val,type))
+
 
 for i in range(n8):
     if btr_8:
-        sum += heapq.heappop(btr_8)
+        cnt+=1
+        sum += heapq.heappop(btr_8)[0]
+
+for i in range(len(btr_8)):
+    val ,type = btr_8[i]
+    heapq.heappush(btr,(val,type))
+
+for i in range(mix):
+    if btr:
+        cnt+=1
+        sum += heapq.heappop(btr)[0]
+
+
+print(cnt , sum)
