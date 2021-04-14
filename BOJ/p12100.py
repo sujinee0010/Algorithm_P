ed20a024a209e279e7985ea6148bca696cce09f1
sujin <sujinee9797@gmail.com>
2021-04-14 20:54:05 +0900

    20200414_12100_2048(Easy)
    구현

--- BOJ/p12100.py
+++ BOJ/p12100.py
@@ -8,78 +8,95 @@ for i in range(n):
     arr.append(list(map(int, sys.stdin.readline().split())))
 
 
+
 def makeArr(cnt, arr, n, s):
     ans = 0
     if cnt == 5:
         return max(map(max, arr))
     else:
-        p = copy.deepcopy(arr)
-        # left
-        for i in range(n):
-            idx = 0
-            for j in range(1, n):
-                tmp, p[i][j] = p[i][j], 0
-                if p[i][idx] == 0:
-                    p[i][idx] = tmp
-                elif tmp == p[i][idx]:
-                    p[i][idx] = tmp * 2
-                    idx += 1
-                else:
-                    idx += 1
-                    p[i][idx] = tmp
-
-        ans = max(makeArr(cnt + 1, p, n, s + "-left"), ans)
-
-        p = copy.deepcopy(arr)
+        a = copy.deepcopy(arr)
         # right
         for i in range(n):
             idx = n - 1
             for j in range(n - 2, -1, -1):
-                tmp, p[i][j] = p[i][j], 0
-                if p[i][idx] == 0:
-                    p[i][idx] = tmp
-                elif tmp == p[i][idx]:
-                    p[i][idx] = tmp * 2
-                    idx -= 1
+                if a[i][j]: # 0 아닐떄 
+                    temp = a[i][j]
+                    a[i][j] = 0
+                    if a[i][idx] == 0:
+                        a[i][idx] = temp
+                    elif a[i][idx] == temp:
+                        a[i][idx] = temp * 2
+                        idx -= 1
+                    else:
+                        idx -= 1
+                        a[i][idx] = temp
                 else:
-                    idx -= 1
-                    p[i][idx] = tmp
+                    print(i , j,a[i][j])
 
-        ans = max(makeArr(cnt + 1, p, n, s + "-right"), ans)
+        ans = max(makeArr(cnt + 1, a, n, s + "-left"), ans)
 
-        p = copy.deepcopy(arr)
-        # up
+        # left
+        a = copy.deepcopy(arr)
+        for i in range(n):
+            idx = 0
+            for j in range(1, n):
+                if a[i][j]:
+                    temp = a[i][j]
+                    a[i][j] = 0
+                    if a[i][idx] == 0:
+                        a[i][idx] = temp
+                    elif a[i][idx] == temp:
+                        a[i][idx] = temp * 2
+                        idx += 1
+                    else:
+                        idx += 1
+                        a[i][idx] = temp
+                else:
+                    print(i , j)
+
+
+        ans = max(makeArr(cnt + 1, a, n, s + "-right"), ans)
+
+        # down
+        a = copy.deepcopy(arr)
+        for j in range(n):
+            idx = n - 1
+            for i in range(n - 2, -1, -1):
+                if a[i][j]:
+                    temp = a[i][j]
+                    a[i][j] = 0
+                    if a[idx][j] == 0:
+                        a[idx][j] = temp
+                    elif a[idx][j] == temp:
+                        a[idx][j] = temp * 2
+                        idx -= 1
+                    else:
+                        idx -= 1
+                        a[idx][j] = temp
+                else:
+                    print(i , j)
+
+        ans = max(makeArr(cnt + 1, a, n, s + "-up"), ans)
+        a = copy.deepcopy(arr)
+        #up
         for j in range(n):
             idx = 0
             for i in range(1, n):
-                tmp, p[i][j] = p[i][j], 0
-                if p[idx][j] == 0:
-                    p[idx][j] = tmp
-                elif tmp == p[idx][j]:
-                    p[idx][j] = tmp * 2
-                    idx += 1
+                if a[i][j]:
+                    temp = a[i][j]
+                    a[i][j] = 0
+                    if a[idx][j] == 0:
+                        a[idx][j] = temp
+                    elif a[idx][j] == temp:
+                        a[idx][j] = temp * 2
+                        idx += 1
+                    else:
+                        idx += 1
+                        a[idx][j] = temp
                 else:
-                    idx += 1
-                    p[idx][j] = tmp
+                    print(i, j)
 
-        ans = max(makeArr(cnt + 1, p, n, s + "-up"), ans)
-        p = copy.deepcopy(arr)
-
-        # down
-        for j in range(n):
-            idx = 0
-            for i in range(n - 2, -1, -1):
-                tmp, p[i][j] = p[i][j], 0
-                if p[idx][j] == 0:
-                    p[idx][j] = tmp
-                elif tmp == p[idx][j]:
-                    p[idx][j] = tmp * 2
-                    idx -= 1
-                else:
-                    idx -= 1
-                    p[idx][j] = tmp
-
-        ans = max(makeArr(cnt + 1, p, n, s + "-down"), ans)
+        ans = max(makeArr(cnt + 1, a, n, s + "-down"), ans)
         return ans
 
 
