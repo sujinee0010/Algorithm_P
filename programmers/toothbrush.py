949629c6ace0e93808adcf9076212659a453f8e9
sujin <sujinee9797@gmail.com>
2021-05-23 11:12:41 +0900

    210523_programmers_다단계칫솔판매
    트레&맵

--- programmers/toothbrush.py
+++ programmers/toothbrush.py
@@ -1,26 +1,21 @@
 def solution(enroll, referral, seller, amount):
-    answer = []
+    answer = [0] * len(enroll)
     roots, money = dict(), dict()
     roots['-'] = 'root'
 
-    for i in range(len(enroll)):
-        money[enroll[i]] = 0
-        roots[enroll[i]] = referral[i]
+    for i, name in enumerate(enroll):
+        money[name] = i
+        roots[name] = referral[i]
 
     for i in range(len(seller)):
         sperson = seller[i]
         samount = amount[i] * 100
         root = roots[sperson]
-
-        # 분배
         while sperson != '-' and samount > 0:
             sub_amount = int(samount * 0.1) if int(samount * 0.1) >= 1 else 0
-            money[sperson] += samount - sub_amount
+            answer[money[sperson]] += samount - sub_amount
             samount = sub_amount
             sperson = root
             root = roots[sperson]
-
-    for m in money.values():
-        answer.append(m)
 
     return answer
