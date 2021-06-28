1d506291168b7c433a1ec6a01cc81f8da9b8d805
sujin <sujinee9797@gmail.com>
2021-06-29 00:54:20 +0900

    210628_11066_파일합치기
    dp

--- BOJ/11066.py
+++ BOJ/11066.py
@@ -1,37 +1,22 @@
-import heapq
 import sys
 
-def dijkstra(files, start, cnt, end):
-    node_list = [[] for _ in range(cnt + 1)]
-    INF = int(1e9)
-    d = [INF for _ in range(cnt + 1)]
-    h = []
-    for g in graph:
-        n1, n2, val = g
-        node_list[n1].append((n2, val))
+input = sys.stdin.readline
 
-    # 비용 , 노드 순 - 비용 작은순으로 정렬
-    heapq.heappush(h, (0, start))
-    d[start] = 40
+t = int(input())
+for __ in range(t):
+    k = int(input())
+    page = list(map(int, input().split()))
 
-    while h:
-        val, n1 = heapq.heappop(h)
+    table = [[0] * k for _ in range(k)]
+    for i in range(k - 1):
+        table[i][i + 1] = page[i] + page[i + 1]
+        for j in range(i + 2, k):
+            table[i][j] = table[i][j - 1] + page[j]
 
-        for next in node_list[n1]:
-            n2, nval = next
-            if d[n1] + nval < d[n2]:
-                d[n2] = d[n1] + nval
-                heapq.heappush(h, (d[n2], n2))
+    for d in range(2, k):  # diagonal
+        for i in range(k - d):
+            j = i + d
+            minimum = [table[i][k] + table[k + 1][j] for k in range(i, j)]
+            table[i][j] += min(minimum)
 
-    return d[end]
-
-T = int(sys.stdin.readline())
-for _ in range(T):
-    n = int(sys.stdin.readline())
-    files = list(map(int , sys.stdin.readline().split()))
-    graph = [[1, 2, 30], [2, 3, 30], [3, 4, 50]]
-
-    print(dijkstra(graph, 1, 4, 4))
-
-
-
+    print(table[0][k - 1])
